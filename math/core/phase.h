5026f5fcb6e951d51341f798113a77b2d9f2673f
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-12-14 14:10:02 +0000

    Added macro to compute the geometric phase for a conventional spherical system.

--- math/core/phase.h
+++ math/core/phase.h
@@ -8,9 +8,17 @@
 /**
  * @brief
  * Inline function macro used to compute the 2D geometric phase
- * for the horizontal coordinate system.
+ * for the horizontal (azimuth/elevation) coordinate system.
  */
 #define GEOMETRIC_PHASE_2D_HORIZONTAL(x, y, cosEl, sinAz, cosAz, k) \
-    (-k * cosEl * (x * sinAz + y * cosAz))
+        (-k * cosEl * (x * sinAz + y * cosAz))
+
+/**
+ * @brief
+ * Inline function macro used to compute the 2D geometric phase
+ * for the spherical (theta/phi) coordinate system.
+ */
+#define GEOMETRIC_PHASE_2D_SPHERICAL(x, y, sinTheta, cosPhi, sinPhi, k) \
+        (-k * sinTheta * (x * cosPhi + y * sinPhi))
 
 #endif /* PHASE_H_ */
