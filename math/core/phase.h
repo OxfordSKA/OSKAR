21bfff411782bc0cbec2b09c80825f593c6b39c3
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2011-02-21 17:09:31 +0000

    Added extra coordinate systems for geometric phase calculations.

--- math/core/phase.h
+++ math/core/phase.h
@@ -37,26 +37,132 @@
  * @brief
  * Inline function macro used to compute the 2D geometric phase
  * for the horizontal (azimuth/elevation) coordinate system.
+ *
+ * - Antenna positions are specified in the horizontal xy-plane, where the
+ *   x-axis points to geographic East, y to geographic North, and z to the
+ *   zenith.
+ * - Azimuth is the angle measured from the y-axis (North) towards the
+ *   x-axis (East).
+ * - Elevation is the angle measured from the xy-plane towards the z-axis,
+ *   which points to the zenith.
  */
 #define GEOMETRIC_PHASE_2D_HORIZONTAL(x, y, cosEl, sinAz, cosAz, k) \
         (-k * cosEl * (x * sinAz + y * cosAz))
 
 /**
  * @brief
+ * Inline function macro used to compute the 2D geometric phase
+ * for the horizontal (azimuth/elevation) coordinate system.
+ *
+ * Positions are assumed to be already multiplied by the wavenumber.
+ *
+ * - Antenna positions are specified in the horizontal xy-plane, where the
+ *   x-axis points to geographic East, y to geographic North, and z to the
+ *   zenith.
+ * - Azimuth is the angle measured from the y-axis (North) towards the
+ *   x-axis (East).
+ * - Elevation is the angle measured from the xy-plane towards the z-axis,
+ *   which points to the zenith.
+ */
+#define GEOMETRIC_PHASE_2D_HORIZONTAL_K(x, y, cosEl, sinAz, cosAz) \
+        (-cosEl * (x * sinAz + y * cosAz))
+
+/**
+ * @brief
+ * Inline function macro used to compute the 2D geometric phase
+ * for the spherical (phi/theta) coordinate system.
+ *
+ * - Antenna positions are specified in the horizontal xy-plane, where the
+ *   x-axis points to geographic East, y to geographic North, and z to the
+ *   zenith.
+ * - Phi is the angle measured from the x-axis towards the y-axis.
+ * - Theta is the angle measured from the z-axis (the zenith angle).
+ */
+#define GEOMETRIC_PHASE_2D_SPHERICAL(x, y, sinTheta, cosPhi, sinPhi, k) \
+        (-k * sinTheta * (x * cosPhi + y * sinPhi))
+
+/**
+ * @brief
+ * Inline function macro used to compute the 2D geometric phase
+ * for the spherical (phi/theta) coordinate system.
+ *
+ * Positions are assumed to be already multiplied by the wavenumber.
+ *
+ * - Antenna positions are specified in the horizontal xy-plane, where the
+ *   x-axis points to geographic East, y to geographic North, and z to the
+ *   zenith.
+ * - Phi is the angle measured from the x-axis towards the y-axis.
+ * - Theta is the angle measured from the z-axis (the zenith angle).
+ */
+#define GEOMETRIC_PHASE_2D_SPHERICAL_K(x, y, sinTheta, cosPhi, sinPhi) \
+        (-sinTheta * (x * cosPhi + y * sinPhi))
+
+/**
+ * @brief
  * Inline function macro used to compute the 3D geometric phase
  * for the horizontal (azimuth/elevation) coordinate system.
  *
- * TODO needs checking!
+ * - Antenna positions are specified in 3D space, where the x-axis points to
+ *   geographic East, y to geographic North, and z to the zenith.
+ * - Azimuth is the angle measured from the y-axis (North) towards the
+ *   x-axis (East).
+ * - Elevation is the angle measured from the xy-plane towards the z-axis,
+ *   which points to the zenith.
  */
 #define GEOMETRIC_PHASE_3D_HORIZONTAL(x, y, z, sinEl, cosEl, sinAz, cosAz, k) \
         (-k * (cosEl * (x * sinAz + y * cosAz) + z * sinEl))
 
 /**
  * @brief
- * Inline function macro used to compute the 2D geometric phase
- * for the spherical (theta/phi) coordinate system.
+ * Inline function macro used to compute the 3D geometric phase
+ * for the horizontal (azimuth/elevation) coordinate system.
+ *
+ * Positions are assumed to be already multiplied by the wavenumber.
+ *
+ * - Antenna positions are specified in 3D space, where the x-axis points to
+ *   geographic East, y to geographic North, and z to the zenith.
+ * - Azimuth is the angle measured from the y-axis (North) towards the
+ *   x-axis (East).
+ * - Elevation is the angle measured from the xy-plane towards the z-axis,
+ *   which points to the zenith.
  */
-#define GEOMETRIC_PHASE_2D_SPHERICAL(x, y, sinTheta, cosPhi, sinPhi, k) \
-        (-k * sinTheta * (x * cosPhi + y * sinPhi))
+#define GEOMETRIC_PHASE_3D_HORIZONTAL_K(x, y, z, sinEl, cosEl, sinAz, cosAz) \
+        (-(cosEl * (x * sinAz + y * cosAz) + z * sinEl))
+
+/**
+ * @brief
+ * Inline function macro used to compute the 3D geometric phase
+ * for the local equatorial (hour angle/declination) coordinate system.
+ *
+ * - Antenna positions are specified in 3D space, where the x-axis points to
+ *   the local meridian, y towards the East, and z along the Earth's axis of
+ *   rotation towards the North Celestial Pole.
+ * - The Hour Angle is the angle measured towards the West from the observer's
+ *   meridian, but in the equatorial system.
+ * - The Declination is the angle measured from the Earth's equator towards the
+ *   North Celestial Pole.
+ */
+#define GEOMETRIC_PHASE_3D_LOCAL_EQUATORIAL(x, y, z, \
+        sinDec, cosDec, sinHA, cosHA, k) \
+        (-k * (cosDec * (x * cosHA - y * sinHA) + z * sinDec))
+
+/**
+ * @brief
+ * Inline function macro used to compute the 3D geometric phase
+ * for the local equatorial (hour angle/declination) coordinate system.
+ *
+ * Positions are assumed to be already multiplied by the wavenumber.
+ *
+ * - Antenna positions are specified in 3D space, where the x-axis points to
+ *   the local meridian, y towards the East, and z along the Earth's axis of
+ *   rotation towards the North Celestial Pole.
+ * - The Hour Angle is the angle measured towards the West from the observer's
+ *   meridian, but in the equatorial system.
+ * - The Declination is the angle measured from the Earth's equator towards the
+ *   North Celestial Pole.
+ */
+#define GEOMETRIC_PHASE_3D_LOCAL_EQUATORIAL_K(x, y, z, \
+        sinDec, cosDec, sinHA, cosHA) \
+        (-(cosDec * (x * cosHA - y * sinHA) + z * sinDec))
 
 #endif // OSKAR_PHASE_H_
