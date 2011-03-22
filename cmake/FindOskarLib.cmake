077c125ea2cb92edcd3c0287c4c2a7d6b7260517
Benjamin Mort <ben.mort@gmail.com>
2011-03-22 01:48:10 +0000

    added template for new find oskar-lib macro

--- cmake/FindOskarLib.cmake
+++ cmake/FindOskarLib.cmake
@@ -1,39 +1,157 @@
-# Find oskar-lib
-# Find the oskar-lib includes and library
+# - Find oskar-lib
+# =============================================================================
 #
-# Defines the following variables:
+# This module and be used to find oskar-lib and define a number of key variables
+# required for building and linking a project using this library.
 #
-#  OskarLib_FOUND         = True if oskar-lib found
-#  OskarLib_LIBRARIES     = Set of libraries required for linking
-#  OskarLib_INCLUDE_DIR   = top level header directory.
+# Typical usage could be something like:
+#
+#   find_package(OskarLib 0.1 COMPONENTS oskar_widgets oskar_cuda)
+#   add_executable(myexe main.cpp)
+#   target_link_libraries(myexe ${OSKAR_LIBRARIES})
+#
+# =============================================================================
+# The following is a list of variables set:
+#
+#  OSKAR_FOUND                  True if oskar-lib found.
+#
+#  OSKAR_VERSION_MAJOR          The major version of oskar-lib found.
+#  OSKAR_VERSION_MINOR          The minor version of oskar-lib found.
+#  OSKAR_VERSION_PATCH          The patch version of oskar-lib found.
+#
+#  OSKAR_CUDA_FOUND             True if oskar_cuda module was found.
+#  OSKAR_MS_FOUND               True if oskar_ms module was found.
+#  OSKAR_WIDGETS_FOUND          True if oskar_widgets module was found.
+#  OSKAR_MATH_FOUND             True if oskar_math module was found.
+#
+#  OSKAR_INCLUDES               List of paths to all include directories.
+#
+#  OSKAR_INCLUDE_DIR            Path to "include" of oskar-lib.
+#  OSKAR_CUDA_INCLUDE_DIR       Path to "include/cuda".
+#  OSKAR_MS_INCLUDE_DIR         Path to "include/ms".
+#  OSKAR_WIDGETS_INCLUDE_DIR    Path to "include/widgets".
+#  OSKAR_MATH_INCLUDE_DIR       Path to "include/math".
+#
+#  OSKAR_LIBRARY_DIR            Path to "lib" of oskar-lib.
+#
+#  OSKAR_LIBRARIES              Complete list of libararies for using oskar-lib.
+#
+#  OSKAR_CUDA_LIBRARY           The oskar_cuda library.
+#  OSKAR_MS_LIBRARY             The oskar_ms library.
+#  OSKAR_MATH_LIBRARY           The oskar_math library.
+#  OSKAR_WIDGETS_LIBRARY        The oskar_widgets library.
+#
+# FIXME: mmm how to deal with external libs...
+#=========================================
+# -- link tables should be set up correctly for sharded version
+# -- what about static version...
+# -- should still check for libs to get correct error messages before linking?
 
-include(FindPackageHandleStandardArgs)
 
-find_path(OskarLib_INCLUDE_DIR oskar
+##############################
+#
+# General variables.
+#
+##############################
+set(OSKAR_MODULES ms cuda widgets math)
+
+
+
+##############################
+#
+# Version variables.
+#
+##############################
+
+# Check version against that specified.
+set(OSKAR_INSTALLED_VERSION_TOO_OLD false)
+
+# We need at least version 0.0.0!
+if (NOT OSKAR_MIN_VERSION)
+    set(OSKAR_MIN_VERSION "0.0.0")
+endif ()
+
+# Now parse the parts of the user given version string into variables
+string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.[0-9]+" req_oskar_major_vers
+    "${OSKAR_MIN_VERSION}")
+if (NOT req_oskar_major_ver)
+    message(FATAL_ERROR "Invalid version of oskar-lib string was given:
+            \"${OSKAR_MIN_VERSION}\", expected e.g. \"0.1.2\"")
+endif ()
+
+# Now parse the parts of the user given version string into variables
+string(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+" "\\1" req_oskar_major_vers
+     "${OSKAR_MIN_VERSION}")
+string(REGEX REPLACE "^[0-9]+\\.([0-9])+\\.[0-9]+" "\\1" req_oskar_minor_vers
+    "${OSKAR_MIN_VERSION}")
+string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+)" "\\1" req_oskar_patch_vers
+    "${OSKAR_MIN_VERSION}")
+
+# Get the version string from the library...?
+
+
+##############################
+#
+# Include variables.
+#
+##############################
+
+find_path(OSKAR_INCLUDE_DIR oskar
           PATHS
           /usr/include/
           /usr/local/include/
 )
 
-set(OskarLib_NAMES
-    oskar_cuda
-    oskar_widgets_plotting
-)
 
-foreach (lib ${OskarLib_NAMES})
-    find_library(OskarLib_${lib}_LIBRARY NAMES ${lib})
-    list(APPEND OskarLib_LIBRARIES ${OskarLib_${lib}_LIBRARY})
+
+foreach (module ${OSKAR_MODULES})
+    string(TOUPPER ${module} _upper_oskar_module)
+    find_path(OSKAR_${_upper_oskar_module}_INCLUDE_DIR ${module}
+        PATHS
+        ${OSKAR_INCLUDE_DIR}
+    )
 endforeach ()
 
-find_package_handle_standard_args(OskarLib DEFAULT_MSG
-    OskarLib_LIBRARIES OskarLib_INCLUDE_DIR)
+mark_as_advanced(OSKAR_INCLUDE_DIR)
 
-#message(STATUS "AAAAAAAAA ${OskarLib_LIBRARIES}")
 
-#if (NOT OskarLib_FOUND)
-#    set(OskarLib_LIBRARIES)
-#endif ()
 
-#message(STATUS "AAAAAAAAA ${OskarLib_LIBRARIES}")
 
-mark_as_advanced(OskarLib_LIBRARIES OskarLib_INCLUDE_DIR)
+##############################
+#
+# Library variables.
+#
+##############################
+
+foreach (module ${OSKAR_MODULES})
+    string(TOUPPER ${module} _upper_oskar_module)
+    find_library(OSKAR_${_upper_oskar_module}_LIBRARY NAMES "oskar_${module}")
+    list(APPEND OSKAR_LIBRARIES ${OSKAR_${_upper_oskar_module}_LIBRARY})
+endforeach ()
+
+mark_as_advanced(OSKAR_LIBRARIES)
+
+
+
+##############################
+#
+# Check external dependencies.
+#
+##############################
+
+# TODO!
+
+
+
+
+##############################
+#
+# Decide if oskar-lib was found.
+#
+##############################
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OSKAR DEFAULT_MSG
+    OSKAR_LIBRARIES OSKAR_INCLUDE_DIR)
+
+
