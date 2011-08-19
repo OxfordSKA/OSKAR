c3471bd7c2d06b6236c197e560ed2a56f99d8067
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2011-08-19 16:15:21 +0000

    Fixed uninitialised pointer loading antenna pattern data.

--- station/src/oskar_load_antenna_pattern.c
+++ station/src/oskar_load_antenna_pattern.c
@@ -32,6 +32,12 @@ int oskar_load_antenna_pattern(const char* filename, oskar_AntennaData* data)
     if (!fgets(line, sizeof(line), file)) return 1;
     const char* dbi = strstr(line, "dBi"); // Check for presence of "dBi".
 
+    // Initalise pointers to NULL.
+    data->g_phi = NULL;
+    data->g_theta = NULL;
+    data->phi = NULL;
+    data->theta = NULL;
+
     // Loop over and read each line in the file.
     while (fgets(line, sizeof(line), file))
     {
