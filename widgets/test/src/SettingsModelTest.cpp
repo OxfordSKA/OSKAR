736f9dcfd9779991830049b8ed228faa99f6276a
Benjamin Mort <ben.mort@gmail.com>
2011-09-12 00:42:09 +0000

    updated build for model widgets testing

--- widgets/test/src/SettingsModelTest.cpp
+++ widgets/test/src/SettingsModelTest.cpp
@@ -1,6 +1,8 @@
 #include <QtGui/QApplication>
 #include <QtGui/QTreeView>
+#include <QtCore/QString>
 #include <cstdio>
+#include <cstdlib>
 
 #include "widgets/oskar_SettingsModel.h"
 
@@ -9,10 +11,8 @@ int main(int argc, char** argv)
     QApplication app(argc, argv);
 
 
-    oskar_SettingsModel settings_model(0);
-
-
-
+    QString a = "hello";
+    oskar_SettingsModel settings_model(a, 0);
 
     QTreeView tree_view;
     tree_view.setModel(&settings_model);
