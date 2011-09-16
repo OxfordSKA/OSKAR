4878f15c75493f19001c77762b3aeb8b153fba40
Benjamin Mort <ben.mort@gmail.com>
2011-09-16 02:59:54 +0000

    addressed memory leak when calling matlab due to use of clear all

--- sky/matlab/oskar_mex_ra_dec_to_az_el_f.cu
+++ sky/matlab/oskar_mex_ra_dec_to_az_el_f.cu
@@ -5,8 +5,6 @@
 void mexFunction(int /*num_outputs*/, mxArray ** output, int num_inputs,
         const mxArray ** input)
 {
-//    cudaDeviceReset();
-
     // Parse Inputs.
     if (num_inputs != 4)
         mexErrMsgTxt("Four inputs required ==> (ra_rad, dec_rad, lst_rad, lat_rad)");
