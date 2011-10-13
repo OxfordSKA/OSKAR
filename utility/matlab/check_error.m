87aa16cdeb74565e4ce63e7e4ee5f9db4135a7c9
Benjamin Mort <ben.mort@gmail.com>
2011-10-13 23:37:28 +0000

    updated cuda utility mex function

--- utility/matlab/check_error.m
+++
@@ -1,8 +0,0 @@
-%OSKAR_CHECK_CUDA_ERROR Checks the current cuda error code.
-%   code = OSKAR_CHECK_CUDA_ERROR()
-%%
-% See also: mex
-
-%   Copyright 2011 University of Oxford
-%   $Revision: 0.0.0 $
-%   Built-in function.
