74331300309fc78a1d3e8bb9e3f5009020cfdf45
Benjamin Mort <ben.mort@gmail.com>
2011-11-02 13:20:22 +0000

    added method to get an oskar Mem structure to set of jones matrices for a station

---
+++ math/oskar_jones_get_station_pointer.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2011, The University of Oxford
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the University of Oxford nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef OSKAR_JONES_GET_STATION_POINTER_H_
+#define OSKAR_JONES_GET_STATION_POINTER_H_
+
+/**
+ * @file oskar_jones_get_station_pointer.h
+ */
+
+#include "oskar_global.h"
+#include "math/oskar_Jones.h"
+#include "utility/oskar_Mem.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**
+ * @brief Returns a pointer (contained in an oskar_Mem) to the set of Jones
+ * matrices for a specified station index.
+ *
+ * @param[out] J_station       oskar_Mem pointer to the set of Jones matrices
+ *                             for the specified station.
+ * @param[in]  J               OSKAR Jones structure containing Jones matrices
+ *                             for a number of stations and sources.
+ * @param[in]  station_index   Station index in \p J.
+ *
+ * @return error code.
+ */
+OSKAR_EXPORT
+int oskar_jones_get_station_pointer(oskar_Mem* J_station, const oskar_Jones* J,
+        const int station_index);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // OSKAR_JONES_GET_STATION_POINTER_H_
