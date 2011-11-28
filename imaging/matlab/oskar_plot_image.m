78964d664632dd775b8f58cced6d2338a903b45d
Benjamin Mort <ben.mort@gmail.com>
2011-11-28 18:07:46 +0000

    updated matlab image plotting function

--- imaging/matlab/oskar_plot_image.m
+++ imaging/matlab/oskar_plot_image.m
@@ -26,23 +26,23 @@ end
 
 % Set up the figure.
 handle = figure(fig_id);
-screen_size = get(0, 'ScreenSize');
-set(gcf, 'Position', [(screen_size(3) - 700) (screen_size(4) - 700) 625 625]);
+%screen_size = get(0, 'ScreenSize');
+%set(gcf, 'Position', [(screen_size(3) - 700) (screen_size(4) - 700) 625 625]);
 %set(gcf, 'Name', fig_title, 'NumberTitle', 'off');
 set(gcf, 'Color',  'black');
-width  = 700;
-height = 700;
-%screen_size = get(0, 'ScreenSize');
+width  = 800;
+height = 600;
+screen_size = get(0, 'ScreenSize');
 set(gcf,'InvertHardcopy','off');
 set(gcf, 'PaperUnits', 'points');
 set(gcf, 'PaperSize', [width height]);
 left   = (screen_size(3) - (width  + 50));
-bottom = (screen_size(4) - (height + 150));
+bottom = (screen_size(4) - (height + 75));
 set(gcf, 'Position', [left bottom width height]);
 set(gcf,'PaperPositionMode','auto');
 set(gcf, 'PaperPosition', [left bottom width height]);
-%set(gcf, 'Menubar',  'none');
-%set(gcf, 'Toolbar',  'none');
+set(gcf, 'Menubar',  'none');
+set(gcf, 'Toolbar',  'none');
 axis equal;
 
 % Plot the image.
@@ -55,23 +55,22 @@ set(gca, 'XDir', 'reverse');
 set(gca, 'XColor', 'white');
 set(gca, 'YColor', 'white');
 set(gca, 'ZColor', 'white');
-xlabel('Relative RA * cos(Dec) [deg]', 'FontSize', 14, 'FontName', 'Arial');
-ylabel('Relative Dec [deg]', 'FontSize', 14, 'FontName', 'Arial');
+xlabel('Relative RA * cos(Dec) [deg]', 'FontSize', 11, 'FontName', 'Arial');
+ylabel('Relative Dec [deg]', 'FontSize', 11, 'FontName', 'Arial');
 %title([fig_title ...
 %    ' (' num2str(size(Image, 1)) 'x' num2str(size(Image, 2)) ')'], ...
 %    'FontSize', 16, 'Color', 'white', 'FontName', 'Arial');
 
 title([fig_title], ...
-    'FontSize', 16, 'Color', 'white', 'FontName', 'Arial');
+    'FontSize', 14, 'Color', 'white', 'FontName', 'Arial');
 
-
-c = colorbar;
-colormap(jet(1024));
 %caxis([-2e5 0.35*7e6]);
 %caxis(caxis) freeze
 %max(Image(:))
 %min(Image(:))
-ylabel(c, 'Brightness', 'FontName', 'Arial', 'FontSize', 12);
+c = colorbar;
+colormap(jet(1024));
+ylabel(c, 'Brightness', 'FontName', 'Arial', 'FontSize', 11);
 axis square;
 grid on;
 %set(gca, 'GridLineStyle','-');
