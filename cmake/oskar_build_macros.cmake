b8af667cff703ba66a1adb38a06788259586dc29
Benjamin Mort <ben.mort@gmail.com>
2012-04-19 16:36:25 +0000

    fixed a problem retrieving the svn revision from svn info

--- cmake/oskar_build_macros.cmake
+++ cmake/oskar_build_macros.cmake
@@ -185,7 +185,8 @@ endmacro(OSKAR_MEX)
 macro(get_svn_revision dir variable)
     find_program(SVN_EXECUTABLE svn DOC "subversion command line client")
     if (SVN_EXECUTABLE AND EXISTS ${OSKAR_SOURCE_DIR}/.svn)
-        execute_process(COMMAND ${SVN_EXECUTABLE} info ${dir}
+        execute_process(COMMAND 
+            ${SVN_EXECUTABLE} info ${dir}/oskar_global.h
             OUTPUT_VARIABLE ${variable}
             OUTPUT_STRIP_TRAILING_WHITESPACE)
         string(REGEX REPLACE "^(.*\n)?Revision: ([^\n]+).*"
