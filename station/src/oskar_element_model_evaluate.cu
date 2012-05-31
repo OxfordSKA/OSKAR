2796c04a6bc5fe9abe74e0a3e514ae3269886085
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2012-05-31 01:01:17 +0000

    More sketching out of new element model evaluation function.

--- station/src/oskar_element_model_evaluate.cu
+++ station/src/oskar_element_model_evaluate.cu
@@ -28,6 +28,33 @@
 
 #include "station/oskar_element_model_evaluate.h"
 
+#define PIf 3.14159265358979323846f
+#define PI  3.14159265358979323846
+
+__global__
+void oskar_cudak_hor_lmn_to_modified_theta_phi_f(const int num,
+        const float* l, const float* m, const float* n,
+        const float orientation, float* theta, float* phi)
+{
+    // Get the position ID that this thread is working on.
+    const int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i >= num) return;
+
+    // Get the data.
+    float x = l[i];
+    float y = m[i];
+    float z = n[i];
+
+    // Cartesian to spherical (with orientation offset).
+    float p = atan2f(y, x) - orientation;
+    p = fmodf(p, 2.0f * PIf);
+    if (p < 0) p += 2.0f * PIf; // Phi in range 0 to 2 pi.
+    x = sqrtf(x*x + y*y);
+    y = atan2f(x, z); // Theta.
+    phi[i] = p;
+    theta[i] = y;
+}
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -37,7 +64,7 @@ int oskar_element_model_evaluate(const oskar_ElementModel* model, oskar_Mem* G,
         const oskar_Mem* m, const oskar_Mem* n, oskar_Mem* theta,
         oskar_Mem* phi)
 {
-    int error, type, num_sources;
+    int error, double_precision, num_sources, num_blocks, num_threads;
 
     /* Check that the output array is complex. */
     if (!oskar_mem_is_complex(G->type))
@@ -50,20 +77,64 @@ int oskar_element_model_evaluate(const oskar_ElementModel* model, oskar_Mem* G,
             G->location != OSKAR_LOCATION_GPU)
         return OSKAR_ERR_BAD_LOCATION;
 
-    /* Check if output array is matrix type. */
+    /* Check whether single or double precision. */
+    double_precision = oskar_mem_is_double(G->type);
+
+    /* Get number of sources. */
+    num_sources = l->num_elements;
+
+    /* Evaluate polarised response if output array is matrix type. */
     if (oskar_mem_is_matrix(G->type) && model->polarised)
     {
-    	/* Evaluate polarised response. */
+        if (double_precision)
+        {
 
-    	/* Check if spline data is present. */
+        }
+        else
+        {
+            /* Compute modified theta and phi coordinates for dipole X.
+             * Modify true values using orientation parameter. */
+            num_threads = 256;
+            num_blocks = (num_sources + num_threads - 1) / num_threads;
+            oskar_cudak_hor_lmn_to_modified_theta_phi_f
+            OSKAR_CUDAK_CONF(num_blocks, num_threads) (num_sources,
+                    ((const float*)l->data), ((const float*)m->data),
+                    ((const float*)n->data), 0.5 * PIf - orientation_x,
+                    ((float*)theta->data), ((float*)phi->data));
 
+            /* Check if spline data present for dipole X. */
+            if (model->theta_re_x.coeff.data && model->theta_im_x.coeff.data &&
+                    model->phi_re_x.coeff.data && model->phi_im_x.coeff.data)
+            {
+                /* Evaluate spline pattern for dipole X. */
+            }
+            else
+            {
+                /* Evaluate tapered dipole pattern for dipole X. */
+            }
+
+            /* Compute modified theta and phi coordinates for dipole Y.
+             * Modify true values using orientation parameter. */
+
+            /* Check if spline data present for dipole Y. */
+            if (model->theta_re_y.coeff.data && model->theta_im_y.coeff.data &&
+                    model->phi_re_y.coeff.data && model->phi_im_y.coeff.data)
+            {
+                /* Evaluate spline pattern for dipole Y. */
+            }
+            else
+            {
+                /* Evaluate tapered dipole pattern for dipole Y. */
+            }
+        }
     }
+
+    /* Evaluate scalar response if output array is scalar type. */
     else if (oskar_mem_is_scalar(G->type) && !model->polarised)
     {
-    	/* Evaluate scalar response. */
     }
     else
-    	return OSKAR_ERR_BAD_DATA_TYPE;
+        return OSKAR_ERR_BAD_DATA_TYPE;
 
     return 0;
 }
