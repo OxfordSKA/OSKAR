eaccf5f013d59607e667c6bd3c96d5e822e77328
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2012-09-17 01:28:05 +0000

    Fixed some wayward license comments.

--- sky/src/oskar_sky_model_filter_by_radius.c
+++ sky/src/oskar_sky_model_filter_by_radius.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, The University of Oxford
+ * Copyright (c) 2012, The University of Oxford
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -8,7 +8,7 @@
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or src materials provided with the distribution.
+ *    and/or other materials provided with the distribution.
  * 3. Neither the name of the University of Oxford nor the names of its
  *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
