215dd731482c8d552256288c3688a28d3871a483
Benjamin Mort <ben.mort@gmail.com>
2014-08-13 17:10:50 +0000

    updated example run script. CMake will now install a cleanup script into the build directory

---
+++ cmake/oskar_cleanup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+# Script clean up an OSKAR build directory.
+#
+# WARNING: Must only be run from inside the top level of the build directory
+#
+
+if [ ! "$PWD" == "@PROJECT_BINARY_DIR@" ]; then
+    echo "ERROR: This script MUST be run from the top level build directory."
+    exit 1
+fi
+
+# Remove files and directories created by CMake
+rm -rf CMake*
+rm -f  CPack*
+rm -f  CTest*
+rm -f  Makefile
+rm -f  cmake_install.cmake
+rm -rf Testing
+rm -f install_manifest.txt
+
+# Remove OSKAR source module folders
+rm -rf apps
+rm -rf convert
+rm -rf correlate
+rm -rf element
+rm -rf extern
+rm -rf fits
+rm -rf imaging
+rm -rf interferometry
+rm -rf jones
+rm -rf math
+rm -rf matlab
+rm -rf ms
+rm -rf python
+rm -rf settings
+rm -rf sky
+rm -rf splines
+rm -rf station
+rm -rf utility
+
+# Remove the main OSKAR library
+rm -f  liboskar*
+
+# Remove any example data (zip files or directories)
+rm -rf OSKAR-*-Example-Data
+
+# Remove OS X .DS_Store file
+rm -f  .DS_Store
+
+# Remove install folders (WARNING: Use this option with CARE!!)
+if [ "$1" == "-uninstall" ]; then
+    rm -rf @OSKAR_BIN_INSTALL_DIR@
+    rm -rf @OSKAR_LIB_INSTALL_DIR@
+    rm -rf @OSKAR_INCLUDE_INSTALL_DIR@
+    rm -rf @OSKAR_MATLAB_INSTALL_DIR@
+    rm -rf @OSKAR_PYTHON_INSTALL_DIR@
+fi
+
