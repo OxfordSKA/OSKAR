60bc094951a623da2abffc40d265f9369415029d
Benjamin Mort <ben.mort@gmail.com>
2014-09-17 10:36:33 +0000

    removed invalid initialisation - for some reason windows wanted this...

--- apps/log/src/oskar_log_line.c
+++ apps/log/src/oskar_log_line.c
@@ -36,7 +36,7 @@ extern "C" {
 
 void oskar_log_line(oskar_Log* log, char priority, char symbol)
 {
-    va_list args = 0;
+    va_list args;
     char code = symbol;
     FILE* stream = 0;
     int depth = OSKAR_LOG_LINE;
