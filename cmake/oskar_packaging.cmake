ed42db0c1625d8fab30d0ea696d0fd1f7b65d1ad
Benjamin Mort <ben.mort@gmail.com>
2015-02-23 15:27:01 +0000

    updated macro

--- cmake/oskar_packaging.cmake
+++ cmake/oskar_packaging.cmake
@@ -26,7 +26,7 @@ macro(get_svn_revision dir variable)
     find_program(SVN_EXECUTABLE svn DOC "subversion command line client")
     if (SVN_EXECUTABLE AND EXISTS ${OSKAR_SOURCE_DIR}/.svn)
         execute_process(COMMAND
-            ${SVN_EXECUTABLE} info ${dir}/oskar_global.h
+            ${SVN_EXECUTABLE} info ${dir}/oskar_global.h.in
             OUTPUT_VARIABLE ${variable}
             OUTPUT_STRIP_TRAILING_WHITESPACE)
         string(REGEX REPLACE "^(.*\n)?Revision: ([^\n]+).*"
