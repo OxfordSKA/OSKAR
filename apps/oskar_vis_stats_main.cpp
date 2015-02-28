b498a59344e8b3150071823f88b3fccac25c5722
Benjamin Mort <ben.mort@gmail.com>
2015-02-28 03:05:07 +0000

    fixed vis stats app for scalar more visibility files. Also converted to use oskar_Log

--- apps/oskar_vis_stats_main.cpp
+++ apps/oskar_vis_stats_main.cpp
@@ -52,50 +52,46 @@ int main(int argc, char** argv)
 {
     int status = OSKAR_SUCCESS;
 
-    // Register options ========================================================
+    // Register options.
     oskar_OptionParser opt("oskar_vis_stats", oskar_version_string());
     set_options(opt);
     if (!check_options(opt, argc, argv))
         return OSKAR_FAIL;
 
-    // Retrieve options ========================================================
+    // Retrieve options.
     vector<string> vis_filename = opt.getArgs();
     bool verbose = opt.isSet("-v") ? true : false;
+    int num_vis = (int)vis_filename.size();
+    int disp_width = (num_vis == 1) ? 1 : (int)log10(num_vis-1)+1;
+
+    oskar_Log* log = 0;
+    oskar_log_section(log, 'M', "OSKAR-%s starting at %s.", OSKAR_VERSION_STR,
+            oskar_log_system_clock_string(0));
 
     if (verbose)
     {
-        cout << endl;
-        cout << "-----------------------------------------------------------" << endl;
-        cout << "Number of visibility files = " << vis_filename.size() << endl;
-        for (int i = 0; i < (int)vis_filename.size(); ++i)
-            cout << "  --> " << vis_filename[i] << endl;
-        cout << "-----------------------------------------------------------" << endl;
-        cout << endl;
+        oskar_log_message(log, 'S', 0, "Number of visibility files = %i", num_vis);
+        for (int i = 0; i < num_vis; ++i)
+        {
+            oskar_log_message(log, 'S', 1, "[%0*i] %s", disp_width, i,
+                    vis_filename[0].c_str());
+        }
     }
 
-
-    for (int i = 0; i < (int)vis_filename.size(); ++i)
+    oskar_log_section(log, 'M', "Visibility statistics.");
+    for (int i = 0; i < num_vis; ++i)
     {
-        if (verbose)
-            cout << "Loading visibility file: " << vis_filename[i] << endl;
-
         oskar_Binary* h = oskar_binary_create(vis_filename[i].c_str(), 'r',
                 &status);
         oskar_Vis* vis = oskar_vis_read(h, &status);
         oskar_binary_free(h);
         check_error(status);
+        int vis_type = oskar_mem_type(oskar_vis_amplitude(vis));
         double min = DBL_MAX, max = -DBL_MAX;
         double mean = 0.0, rms = 0.0, var = 0.0, std = 0.0;
         double sum = 0.0, sumsq = 0.0;
         int num_vis = 0, num_zero = 0;
-        if (verbose)
-        {
-            cout << "  No. baselines: " << oskar_vis_num_baselines(vis) << endl;
-            cout << "  No. times: " << oskar_vis_num_times(vis) << endl;
-            cout << "  No. channels: " << oskar_vis_num_channels(vis) << endl;
-        }
-        if (oskar_mem_type(oskar_vis_amplitude(vis)) ==
-                OSKAR_SINGLE_COMPLEX_MATRIX)
+        if (vis_type == OSKAR_SINGLE_COMPLEX_MATRIX)
         {
             float4c* amp = oskar_mem_float4c(oskar_vis_amplitude(vis), &status);
             for (int i = 0, c = 0; c < oskar_vis_num_channels(vis); ++c)
@@ -110,8 +106,7 @@ int main(int argc, char** argv)
                         I.x = 0.5 * (xx.x + yy.x);
                         I.y = 0.5 * (xx.y + yy.y);
                         float absI = sqrtf(I.x*I.x + I.y*I.y);
-                        if (absI < DBL_MIN)
-                            num_zero++;
+                        if (absI < DBL_MIN) num_zero++;
                         if (absI > max) max = absI;
                         if (absI < min) min = absI;
                         sum += absI;
@@ -125,8 +120,7 @@ int main(int argc, char** argv)
             var = sumsq/num_vis - mean*mean;
             std = sqrtf(var);
         }
-        else if (oskar_mem_type(oskar_vis_amplitude(vis)) ==
-                OSKAR_DOUBLE_COMPLEX_MATRIX)
+        else if (vis_type == OSKAR_DOUBLE_COMPLEX_MATRIX)
         {
             double4c* amp = oskar_mem_double4c(oskar_vis_amplitude(vis), &status);
             for (int i = 0, c = 0; c < oskar_vis_num_channels(vis); ++c)
@@ -140,9 +134,58 @@ int main(int argc, char** argv)
                         double2 I;
                         I.x = 0.5 * (xx.x + yy.x);
                         I.y = 0.5 * (xx.y + yy.y);
-                        double absI = std::sqrt(I.x * I.x + I.y * I.y);
-                        if (absI < DBL_MIN)
-                            num_zero++;
+                        double absI = std::sqrt(I.x*I.x + I.y*I.y);
+                        if (absI < DBL_MIN) num_zero++;
+                        if (absI > max) max = absI;
+                        if (absI < min) min = absI;
+                        sum += absI;
+                        sumsq += absI * absI;
+                    }
+                }
+            }
+            num_vis = (int)oskar_mem_length(oskar_vis_amplitude(vis));
+            mean = sum / num_vis;
+            rms = std::sqrt(sumsq / num_vis);
+            var = sumsq/num_vis - mean*mean;
+            std = std::sqrt(var);
+        }
+        else if (vis_type == OSKAR_SINGLE_COMPLEX)
+        {
+            float2* amp = oskar_mem_float2(oskar_vis_amplitude(vis), &status);
+            for (int i = 0, c = 0; c < oskar_vis_num_channels(vis); ++c)
+            {
+                for (int t = 0; t < oskar_vis_num_times(vis); ++t)
+                {
+                    for (int b = 0; b < oskar_vis_num_baselines(vis); ++b, ++i)
+                    {
+                        float2 I = amp[i];
+                        float absI = sqrtf(I.x*I.x + I.y*I.y);
+                        if (absI < DBL_MIN) num_zero++;
+                        if (absI > max) max = absI;
+                        if (absI < min) min = absI;
+                        sum += absI;
+                        sumsq += absI * absI;
+                    }
+                }
+            }
+            num_vis = (int)oskar_mem_length(oskar_vis_amplitude(vis));
+            mean = sum / num_vis;
+            rms = sqrtf(sumsq / num_vis);
+            var = sumsq/num_vis - mean*mean;
+            std = sqrtf(var);
+        }
+        else if (vis_type == OSKAR_DOUBLE_COMPLEX)
+        {
+            double2* amp = oskar_mem_double2(oskar_vis_amplitude(vis), &status);
+            for (int i = 0, c = 0; c < oskar_vis_num_channels(vis); ++c)
+            {
+                for (int t = 0; t < oskar_vis_num_times(vis); ++t)
+                {
+                    for (int b = 0; b < oskar_vis_num_baselines(vis); ++b, ++i)
+                    {
+                        double2 I = amp[i];
+                        double absI = std::sqrt(I.x*I.x + I.y*I.y);
+                        if (absI < DBL_MIN) num_zero++;
                         if (absI > max) max = absI;
                         if (absI < min) min = absI;
                         sum += absI;
@@ -158,18 +201,34 @@ int main(int argc, char** argv)
         }
         else
         {
+            oskar_log_error(log, "Incompatible or invalid visibility data type.");
             return OSKAR_ERR_BAD_DATA_TYPE;
         }
-
-        printf("min, max, mean, rms, std\n");
-        printf("%e, %e, %e, %e, %e\n", min, max, mean, rms, std);
-        printf("number of zero visibilities = %i\n", num_zero);
-        printf("number of non-zero visibilities = %i\n", num_vis-num_zero);
-        printf("percent zero visibilities = %f\n", (double)num_zero/num_vis * 100.0);
+        oskar_log_message(log, 'M', 0, "%s ", vis_filename[i].c_str());
+        if (verbose)
+        {
+            oskar_log_message(log, 'S', 1, "No. baselines : %i",
+                    oskar_vis_num_baselines(vis));
+            oskar_log_message(log, 'S', 1, "No. times     : %i",
+                    oskar_vis_num_times(vis));
+            oskar_log_message(log, 'S', 1, "No. channels  : %i",
+                    oskar_vis_num_channels(vis));
+        }
+        oskar_log_message(log, 'M', 1, "Stokes-I amplitude:");
+        oskar_log_message(log, 'M', 2, "Min.  : %e", min);
+        oskar_log_message(log, 'M', 2, "Max.  : %e", max);
+        oskar_log_message(log, 'M', 2, "Mean  : %e", mean);
+        oskar_log_message(log, 'M', 2, "RMS   : %e", rms);
+        oskar_log_message(log, 'M', 2, "STD   : %e", std);
+        oskar_log_message(log, 'M', 2, "Zeros : %i/%i (%.1f%%)", num_zero,
+                num_vis, (double)(num_zero/num_vis)*100.0);
 
         // Free visibility data.
         oskar_vis_free(vis, &status);
     } // end of loop over visibility files
+
+    oskar_log_section(log, 'M', "OSKAR-%s starting at %s.", OSKAR_VERSION_STR,
+            oskar_log_system_clock_string(0));
 
     return status;
 }
