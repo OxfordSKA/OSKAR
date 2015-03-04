281bf11ac693bbe40e7ba7b81bdda48642131f5c
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2015-03-04 18:34:11 +0000

    Optimisation in oskar_sky_copy() to only copy the number of sources set in the sky model.

--- sky/src/oskar_sky_copy.c
+++ sky/src/oskar_sky_copy.c
@@ -34,9 +34,10 @@
 extern "C" {
 #endif
 
-void oskar_sky_copy(oskar_Sky* dst, const oskar_Sky* src,
-        int* status)
+void oskar_sky_copy(oskar_Sky* dst, const oskar_Sky* src, int* status)
 {
+    int num_sources;
+
     /* Check all inputs. */
     if (!src || !dst || !status)
     {
@@ -47,40 +48,54 @@ void oskar_sky_copy(oskar_Sky* dst, const oskar_Sky* src,
     /* Check if safe to proceed. */
     if (*status) return;
 
-    if (oskar_sky_precision(dst) != oskar_sky_precision(src)) {
+    if (oskar_sky_precision(dst) != oskar_sky_precision(src))
+    {
         *status = OSKAR_ERR_BAD_DATA_TYPE;
         return;
     }
-    if (oskar_sky_capacity(dst) < oskar_sky_capacity(src)) {
+    if (oskar_sky_capacity(dst) < oskar_sky_capacity(src))
+    {
         *status = OSKAR_ERR_DIMENSION_MISMATCH;
         return;
     }
 
     /* Copy meta data */
-    dst->num_sources = src->num_sources;
+    num_sources = src->num_sources;
+    dst->num_sources = num_sources;
     dst->use_extended = src->use_extended;
     dst->reference_ra_rad = src->reference_ra_rad;
     dst->reference_dec_rad = src->reference_dec_rad;
 
     /* Copy the memory blocks */
-    oskar_mem_copy(dst->ra_rad, src->ra_rad, status);
-    oskar_mem_copy(dst->dec_rad, src->dec_rad, status);
-    oskar_mem_copy(dst->I, src->I, status);
-    oskar_mem_copy(dst->Q, src->Q, status);
-    oskar_mem_copy(dst->U, src->U, status);
-    oskar_mem_copy(dst->V, src->V, status);
-    oskar_mem_copy(dst->reference_freq_hz, src->reference_freq_hz, status);
-    oskar_mem_copy(dst->spectral_index, src->spectral_index, status);
-    oskar_mem_copy(dst->rm_rad, src->rm_rad, status);
-    oskar_mem_copy(dst->l, src->l, status);
-    oskar_mem_copy(dst->m, src->m, status);
-    oskar_mem_copy(dst->n, src->n, status);
-    oskar_mem_copy(dst->fwhm_major_rad, src->fwhm_major_rad, status);
-    oskar_mem_copy(dst->fwhm_minor_rad, src->fwhm_minor_rad, status);
-    oskar_mem_copy(dst->pa_rad, src->pa_rad, status);
-    oskar_mem_copy(dst->gaussian_a, src->gaussian_a, status);
-    oskar_mem_copy(dst->gaussian_b, src->gaussian_b, status);
-    oskar_mem_copy(dst->gaussian_c, src->gaussian_c, status);
+    oskar_mem_copy_contents(dst->ra_rad, src->ra_rad,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->dec_rad, src->dec_rad,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->I, src->I, 0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->Q, src->Q, 0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->U, src->U, 0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->V, src->V, 0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->reference_freq_hz, src->reference_freq_hz,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->spectral_index, src->spectral_index,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->rm_rad, src->rm_rad,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->l, src->l, 0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->m, src->m, 0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->n, src->n, 0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->fwhm_major_rad, src->fwhm_major_rad,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->fwhm_minor_rad, src->fwhm_minor_rad,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->pa_rad, src->pa_rad,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->gaussian_a, src->gaussian_a,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->gaussian_b, src->gaussian_b,
+            0, 0, num_sources, status);
+    oskar_mem_copy_contents(dst->gaussian_c, src->gaussian_c,
+            0, 0, num_sources, status);
 }
 
 #ifdef __cplusplus
