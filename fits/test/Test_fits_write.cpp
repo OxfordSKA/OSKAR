b925fb9ac8581d429eee71b96a3e1adecc3b4927
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2015-03-12 16:47:25 +0000

    Extra test for FITS usage.

--- fits/test/Test_fits_write.cpp
+++ fits/test/Test_fits_write.cpp
@@ -29,11 +29,131 @@
 #include <gtest/gtest.h>
 
 #include "fits/oskar_fits_write.h"
+#include "fits/oskar_fits_write_axis_header.h"
+#include <fitsio.h>
+#include <oskar_file_exists.h>
 #include <oskar_mem.h>
 #include <oskar_cmath.h>
 
 #include <cstdio>
 #include <cstdlib>
+
+static double fov_to_cellsize(double fov_deg, int num_pixels)
+{
+    double max, inc;
+    max = sin(fov_deg * M_PI / 360.0); /* Divide by 2. */
+    inc = max / (0.5 * num_pixels);
+    return asin(inc) * 180.0 / M_PI;
+}
+
+static
+fitsfile* create_fits_file(const char* filename, int precision, int width,
+        int height, int num_times, int num_channels, double centre_deg[2],
+        double fov_deg[2], double start_time_mjd, double delta_time_sec,
+        double start_freq_hz, double delta_freq_hz, int* status)
+{
+    int imagetype;
+    long naxes[4];
+    double delta;
+    fitsfile* f = 0;
+
+    /* Create a new FITS file and write the image headers. */
+    if (oskar_file_exists(filename)) remove(filename);
+    imagetype = (precision == OSKAR_DOUBLE ? DOUBLE_IMG : FLOAT_IMG);
+    naxes[0]  = width;
+    naxes[1]  = height;
+    naxes[2]  = num_times;
+    naxes[3]  = num_channels;
+    fits_create_file(&f, filename, status);
+    fits_create_img(f, imagetype, 4, naxes, status);
+    fits_write_date(f, status);
+    fits_write_key_str(f, "TELESCOP",
+            "OSKAR " OSKAR_VERSION_STR, NULL, status);
+    fits_write_history(f, "Created using OSKAR " OSKAR_VERSION_STR, status);
+
+    /* Write axis headers. */
+    delta = fov_to_cellsize(fov_deg[0], width);
+    oskar_fits_write_axis_header(f, 1, "RA---SIN", "Right Ascension",
+            centre_deg[0], -delta, (width + 1) / 2.0, 0.0, status);
+    delta = fov_to_cellsize(fov_deg[1], height);
+    oskar_fits_write_axis_header(f, 2, "DEC--SIN", "Declination",
+            centre_deg[1], delta, (height + 1) / 2.0, 0.0, status);
+    oskar_fits_write_axis_header(f, 3, "UTC", "Time",
+            start_time_mjd, delta_time_sec, 1.0, 0.0, status);
+    oskar_fits_write_axis_header(f, 4, "FREQ", "Frequency",
+            start_freq_hz, delta_freq_hz, 1.0, 0.0, status);
+
+    /* Write other headers. */
+    fits_write_key_str(f, "TIMESYS", "UTC", NULL, status);
+    fits_write_key_str(f, "TIMEUNIT", "s", "Time axis units", status);
+    fits_write_key_dbl(f, "MJD-OBS", start_time_mjd, 10, "Start time", status);
+    fits_write_key_dbl(f, "OBSRA", centre_deg[0], 10, "RA", status);
+    fits_write_key_dbl(f, "OBSDEC", centre_deg[1], 10, "DEC", status);
+
+    return f;
+}
+
+
+TEST(fits_write, test_planes)
+{
+    int status = 0;
+    int width = 10;
+    int height = 40;
+    int num_times = 4;
+    int num_channels = 4;
+    int precision = OSKAR_DOUBLE;
+    oskar_Mem* data = oskar_mem_create(precision, OSKAR_CPU,
+            width * height, &status);
+    const char filename[] = "temp_test_fits_write_planes.fits";
+
+    // Create the FITS file.
+    double phase_centre_deg[2] = {10., 80.};
+    double fov_deg[2] = {1., 4.};
+    fitsfile* f = create_fits_file(filename, precision, width, height,
+            num_times, num_channels, phase_centre_deg, fov_deg,
+            1.0, 1.0, 100e6, 1e5, &status);
+    int datatype = (precision == OSKAR_DOUBLE) ? TDOUBLE : TFLOAT;
+
+    // Define test data.
+    double* d = oskar_mem_double(data, &status);
+    for (int c = 0; c < num_channels; ++c)
+    {
+        for (int t = 0; t < num_times; ++t)
+        {
+            int i = 0; // Pixel index within plane.
+            if (t == 0)
+            {
+                for (int h = 0; h < height; ++h)
+                {
+                    for (int w = 0; w < width; ++w)
+                    {
+                        d[i++] = h + 2 * w;
+                    }
+                }
+            }
+            else
+            {
+                for (int h = 0; h < height; ++h)
+                {
+                    for (int w = 0; w < width; ++w)
+                    {
+                        d[i++] = (c+1) *
+                                sin((t+1) * M_PI * h / (double)(height-1));
+                    }
+                }
+            }
+
+            // Write plane to FITS file.
+            long firstpix[] = {1, 1, t + 1, c + 1};
+            fits_write_pix(f, datatype, firstpix, width * height, d, &status);
+        }
+    }
+
+    // Close the FITS file.
+    fits_close_file(f, &status);
+    oskar_mem_free(data, &status);
+}
+
 
 TEST(fits_write, test)
 {
