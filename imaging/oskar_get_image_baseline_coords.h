a8541a64103024a7a856870603a783de7e477e9f
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2015-05-14 15:03:46 +0000

    Removed unneeded include.

--- imaging/oskar_get_image_baseline_coords.h
+++ imaging/oskar_get_image_baseline_coords.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014, The University of Oxford
+ * Copyright (c) 2012-2015, The University of Oxford
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,7 +37,6 @@
 #include <oskar_global.h>
 
 #include <oskar_mem.h>
-#include "interferometry/oskar_vis.h"
 #include "oskar_SettingsImage.h"
 
 #ifdef __cplusplus
