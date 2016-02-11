e9d031329c2c2d0be0a0a7c4cc9434f6121b4497
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2016-02-11 17:00:30 +0000

    added bda python module utilities to apply gains and convert visibility lists to matrix form needed for StefCal

--- python/__init__.py
+++ python/__init__.py
@@ -1,6 +1,6 @@
-# 
+#
 #  This file is part of OSKAR.
-# 
+#
 # Copyright (c) 2014, The University of Oxford
 # All rights reserved.
 #
@@ -17,7 +17,7 @@
 #  3. Neither the name of the University of Oxford nor the names of its
 #     contributors may be used to endorse or promote products derived from this
 #     software without specific prior written permission.
-# 
+#
 #  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 #  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 #  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -29,7 +29,7 @@
 #  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 #  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 #  POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 """OSKAR is a package to simulate interferometric visibility data ....
 """
 
