3585c4efc20bbd72c83c0b5f3b87448c76d82a01
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2016-06-29 15:57:11 +0100

    reorganised some cmake functions

---
+++ cmake/oskar_includes.cmake
@@ -0,0 +1,35 @@
+include_directories(
+    ${PROJECT_SOURCE_DIR}
+    ${PROJECT_BINARY_DIR}
+    ${PROJECT_SOURCE_DIR}/apps
+    ${PROJECT_SOURCE_DIR}/apps/lib
+    ${PROJECT_SOURCE_DIR}/apps/log
+    ${PROJECT_SOURCE_DIR}/convert
+    ${PROJECT_SOURCE_DIR}/correlate
+    ${PROJECT_SOURCE_DIR}/element
+    ${PROJECT_SOURCE_DIR}/extern
+    ${PROJECT_SOURCE_DIR}/extern/gtest-1.7.0/include
+    ${PROJECT_SOURCE_DIR}/extern/rapidxml-1.13
+    ${PROJECT_SOURCE_DIR}/extern/cfitsio-3.37
+    ${PROJECT_SOURCE_DIR}/extern/Random123
+    ${PROJECT_SOURCE_DIR}/extern/Random123/features
+    ${PROJECT_SOURCE_DIR}/imaging
+    ${PROJECT_SOURCE_DIR}/interferometry
+    ${PROJECT_SOURCE_DIR}/jones
+    ${PROJECT_SOURCE_DIR}/math
+    ${PROJECT_SOURCE_DIR}/ms
+    ${PROJECT_SOURCE_DIR}/settings
+    ${PROJECT_SOURCE_DIR}/settings/containers
+    ${PROJECT_SOURCE_DIR}/settings/load
+    ${PROJECT_SOURCE_DIR}/settings/struct
+    ${PROJECT_SOURCE_DIR}/settings/types
+    ${PROJECT_SOURCE_DIR}/settings/utility
+    ${PROJECT_SOURCE_DIR}/settings/widgets
+    ${PROJECT_SOURCE_DIR}/sky
+    ${PROJECT_SOURCE_DIR}/splines
+    ${PROJECT_SOURCE_DIR}/station
+    ${PROJECT_SOURCE_DIR}/utility
+    ${PROJECT_SOURCE_DIR}/utility/binary
+)
+set(GTEST_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/extern/gtest-1.7.0/include/gtest)
+set(EZOPT_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/extern/ezOptionParser-0.2.0)
---
+++ cmake/oskar_set_version.cmake
@@ -0,0 +1,30 @@
+set(OSKAR_VERSION "${OSKAR_VERSION_MAJOR}.${OSKAR_VERSION_MINOR}.${OSKAR_VERSION_PATCH}")
+set(OSKAR_VERSION_STR "${OSKAR_VERSION}")
+if (OSKAR_VERSION_SUFFIX AND NOT OSKAR_VERSION_SUFFIX STREQUAL "")
+    find_package(Git QUIET)
+    if (GIT_FOUND)
+        execute_process(
+          COMMAND git log -1 --format=%h
+          WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+          OUTPUT_VARIABLE GIT_COMMIT_HASH
+          OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        execute_process(
+          COMMAND git log -1 --format=%cd --date=short
+          WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+          OUTPUT_VARIABLE GIT_COMMIT_DATE
+          OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        set(GIT_COMMIT_INFO "${GIT_COMMIT_DATE} ${GIT_COMMIT_HASH}")
+    endif()
+    set(OSKAR_VERSION_STR "${OSKAR_VERSION}-${OSKAR_VERSION_SUFFIX}")
+    if (GIT_COMMIT_INFO)
+        set(OSKAR_VERSION_STR "${OSKAR_VERSION_STR} ${GIT_COMMIT_INFO}")
+    endif()
+    if (CMAKE_BUILD_TYPE MATCHES [dD]ebug)
+        set(OSKAR_VERSION_STR "${OSKAR_VERSION_STR} -debug-")
+    endif()
+endif()
+
+configure_file(${PROJECT_SOURCE_DIR}/cmake/oskar_version.h.in
+    ${PROJECT_BINARY_DIR}/oskar_version.h @ONLY)
