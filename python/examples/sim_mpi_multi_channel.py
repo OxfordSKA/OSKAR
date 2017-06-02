e48816321b48be02e9996189720b723829d40056
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2017-06-02 16:37:31 +0100

    Added example using mpi4py to simulate multiple channels.

---
+++ python/examples/sim_mpi_multi_channel.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import oskar
+from mpi4py import MPI
+
+if __name__ == '__main__':
+    # Global options.
+    precision = 'single'
+    phase_centre_ra_deg = 0.0
+    phase_centre_dec_deg = -60.0
+
+    # Get MPI communicator and rank, and set values that depend on the rank.
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    frequency_hz = 50e6 + 0.5e6 * rank
+    filename = ('test_data_%05.1f.ms' % (frequency_hz / 1e6))
+
+    # Set up the sky model.
+    sky = oskar.Sky.generate_grid(phase_centre_ra_deg, phase_centre_dec_deg,
+                                  16, 5, precision=precision)
+    sky.append_sources(phase_centre_ra_deg, phase_centre_dec_deg, 1.0)
+
+    # Set up the telescope model.
+    tel = oskar.Telescope(precision)
+    tel.set_channel_bandwidth(100e3)
+    tel.set_time_average(10.0)
+    tel.set_pol_mode('Scalar')
+    tel.load('telescope.tm')  # Set the name of the telescope directory here.
+    # Set station properties after stations have been defined.
+    tel.set_phase_centre(phase_centre_ra_deg, phase_centre_dec_deg)
+    tel.set_station_type('Isotropic')
+
+    # Set up the basic simulator and run simulation.
+    simulator = oskar.Simulator(precision)
+    simulator.set_settings_path(os.path.abspath(__file__))
+    simulator.set_sky_model(sky)
+    simulator.set_telescope_model(tel)
+    simulator.set_observation_frequency(frequency_hz)
+    simulator.set_observation_time(
+        start_time_mjd_utc=51544.375, length_sec=10800.0, num_time_steps=180)
+    simulator.set_output_measurement_set(filename)
+    simulator.run()
