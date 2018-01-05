9b8e0684f95a85d9eb35e37c0e86d5ba4b2a1090
fdulwich <fdulwich@users.noreply.github.com>
2018-01-05 22:19:12 +0000

    Added handles to hold rearranged kernel data.

--- oskar/imager/private_imager.h
+++ oskar/imager/private_imager.h
@@ -99,7 +99,7 @@ struct oskar_Imager
     size_t ww_points;
     int num_w_planes, conv_size_half;
     double w_scale, ww_min, ww_max, ww_rms;
-    oskar_Mem *w_kernels, *w_support;
+    oskar_Mem *w_kernels, *w_support, *w_kernels_compact, *w_kernel_start;
 
     /* Memory allocated per GPU (array of DeviceData structures). */
     DeviceData* d;
