4819c68db77f1e7845c7da3fb175a70a385fad2b
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2019-05-31 22:27:42 +0100

    Fixed script to convert CST to scalar patterns.

--- apps/oskar_convert_cst_to_scalar.py
+++ apps/oskar_convert_cst_to_scalar.py
@@ -3,6 +3,9 @@
 Convert polarised CST element files to OSKAR scalar element pattern format.
 """
 
+from __future__ import print_function
+import sys
+import numpy
 
 def load_cst_file(filename):
     """"
@@ -17,9 +20,6 @@ def load_cst_file(filename):
     -------
     Matrix of values from the CST file.
     """
-
-    import numpy as np
-
     f = open(filename, 'r')
     lines = f.readlines()
     f.close()
@@ -29,9 +29,9 @@ def load_cst_file(filename):
         if not len(values) == 8:
             continue
         else:
-            x_all = np.array(values, dtype=np.double)
+            x_all = numpy.array(values, dtype=numpy.dtype('f8'))
             X.append(x_all)
-    return np.array(X, dtype=np.double)
+    return numpy.array(X, dtype=numpy.dtype('f8'))
 
 
 def convert(cst_file_in, scalar_file_out):
@@ -50,55 +50,52 @@ def convert(cst_file_in, scalar_file_out):
     This function is designed to be used to create scalar element input files
     for the oskar_fit_element_data application.
     """
-
-    import numpy as np
-
-    # Load the CST element pattern data for X. (Ignore lines that don't consist
-    # of 8 floats)
+    # Load the CST element pattern data.
     X = load_cst_file(cst_file_in)
-    # Only require a columns for:
+    # Only require columns for:
     # Theta, Phi, Abs(Theta), Phase(Theta), Abs(Phi), Phase(Phi)
-    X = np.copy(X[:, [0, 1, 3, 4, 5, 6]])
+    X = numpy.copy(X[:, [0, 1, 3, 4, 5, 6]])
+
+    # Discard any data at values of phi >= 360 degrees,
+    # as any duplicated entries will cause this method to fail.
+    X = X[X[:, 1] < 360.0, :]
 
     # Generate the rotated data for Y from X by adding 90 degrees to the phi
     # values
-    Y = np.copy(X)
+    Y = numpy.copy(X)
     Y[:, 1] += 90.0
     Y[Y[:, 1] >= 360.0, 1] -= 360.0
 
     # Linked column sort by phi and then theta for both X and Y.
-    X = X[np.lexsort((X[:, 1], X[:, 0])), :]
-    Y = Y[np.lexsort((Y[:, 1], Y[:, 0])), :]
-    assert(np.sum(X[:, 0] == Y[:, 0]) == len(X[:, 0]))
-    assert(np.sum(X[:, 1] == Y[:, 1]) == len(X[:, 1]))
+    X = X[numpy.lexsort((X[:, 0], X[:, 1])), :]
+    Y = Y[numpy.lexsort((Y[:, 0], Y[:, 1])), :]
+
+    # Check that the coordinate columns in X and Y now match.
+    assert numpy.sum(numpy.abs(X[:, 0] - Y[:, 0])) < 1e-6
+    assert numpy.sum(numpy.abs(X[:, 1] - Y[:, 1])) < 1e-6
 
     # Generate scalar values from sorted data.
-    X_theta = X[:, 2] * np.exp(1j * X[:, 3] * np.pi / 180.0)
-    X_phi = X[:, 4] * np.exp(1j * X[:, 5] * np.pi / 180.0)
-    Y_theta = Y[:, 2] * np.exp(1j * Y[:, 3] * np.pi / 180.0)
-    Y_phi = Y[:, 4] * np.exp(1j * Y[:, 5] * np.pi / 180.0)
-    s = X_theta * np.conj(X_theta) + X_phi * np.conj(X_phi) + \
-        Y_theta * np.conj(Y_theta) + Y_phi * np.conj(Y_phi)
+    X_theta = X[:, 2] * numpy.exp(1j * numpy.radians(X[:, 3]))
+    X_phi = X[:, 4] * numpy.exp(1j * numpy.radians(X[:, 5]))
+    Y_theta = Y[:, 2] * numpy.exp(1j * numpy.radians(Y[:, 3]))
+    Y_phi = Y[:, 4] * numpy.exp(1j * numpy.radians(Y[:, 5]))
+    s = X_theta * numpy.conj(X_theta) + X_phi * numpy.conj(X_phi) + \
+        Y_theta * numpy.conj(Y_theta) + Y_phi * numpy.conj(Y_phi)
 
     # Take the sqrt to convert to a 'voltage'
-    s = np.sqrt(0.5 * s)
-    s_amp = np.absolute(s)
-    s_phase = np.angle(s, deg=True)
+    s = numpy.sqrt(0.5 * s)
+    s_amp = numpy.absolute(s)
+    s_phase = numpy.angle(s, deg=True)
 
     # Write scalar values to file Columns = (theta, phi, amp, phase).
-    o = np.column_stack((X[:, 0], X[:, 1], s_amp, s_phase))
-    np.savetxt(scalar_file_out, o, fmt=['%12.4f', '%12.4f', '%20.6e',
-                                        '%12.4f'])
+    o = numpy.column_stack((X[:, 0], X[:, 1], s_amp, s_phase))
+    numpy.savetxt(scalar_file_out, o,
+                  fmt=['%12.4f', '%12.4f', '%20.6e', '%12.4f'])
 
 if __name__ == "__main__":
-    import sys
-
     if len(sys.argv) < 3:
-        print "Usage: oskar_convert_cst_to_scalar.py " \
-            "<input CST file> <output scalar file>"
+        print("Usage: oskar_convert_cst_to_scalar.py "
+              "<input CST file> <output scalar file>")
         sys.exit(1)
 
-    filename = sys.argv[1]
-    outname = sys.argv[2]
-
-    convert(filename, outname)
+    convert(sys.argv[1], sys.argv[2])
