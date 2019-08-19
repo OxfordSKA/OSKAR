4068d29a0db4f8b0150957f24d61badbf47b18cd
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2019-08-19 22:35:03 +0100

    Preserve station coordinates if duplicating stations.

--- oskar/telescope/src/oskar_telescope_duplicate_first_station.c
+++ oskar/telescope/src/oskar_telescope_duplicate_first_station.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015, The University of Oxford
+ * Copyright (c) 2013-2019, The University of Oxford
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -44,10 +44,19 @@ void oskar_telescope_duplicate_first_station(oskar_Telescope* telescope,
     /* Copy the first station to the others. */
     for (i = 1; i < telescope->num_stations; ++i)
     {
-        oskar_station_free(oskar_telescope_station(telescope, i), status);
+        /* Preserve the coordinates. */
+        const double lon = oskar_station_lon_rad(telescope->station[i]);
+        const double lat = oskar_station_lat_rad(telescope->station[i]);
+        const double alt = oskar_station_alt_metres(telescope->station[i]);
+
+        /* Copy the first station. */
+        oskar_station_free(telescope->station[i], status);
         telescope->station[i] = oskar_station_create_copy(
-                oskar_telescope_station_const(telescope, 0),
+                telescope->station[0],
                 telescope->mem_location, status);
+
+        /* Reinstate the coordinates. */
+        oskar_station_set_position(telescope->station[i], lon, lat, alt);
     }
 }
 
