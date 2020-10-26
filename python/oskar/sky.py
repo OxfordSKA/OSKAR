fa2e922ff931208cf7593d0e51e3826adab5cf91
Fred Dulwich <145223+fdulwich@users.noreply.github.com>
2020-10-26 16:59:27 +0000

    Add method to write a ds9 region file from a sky model.

--- python/oskar/sky.py
+++ python/oskar/sky.py
@@ -1,30 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2016-2020, The University of Oxford
-# All rights reserved.
-#
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions are met:
-#  1. Redistributions of source code must retain the above copyright notice,
-#     this list of conditions and the following disclaimer.
-#  2. Redistributions in binary form must reproduce the above copyright notice,
-#     this list of conditions and the following disclaimer in the documentation
-#     and/or other materials provided with the distribution.
-#  3. Neither the name of the University of Oxford nor the names of its
-#     contributors may be used to endorse or promote products derived from this
-#     software without specific prior written permission.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-#  POSSIBILITY OF SUCH DAMAGE.
+# Copyright (c) 2016-2020, The OSKAR Developers.
+# See the LICENSE file at the top-level directory of this distribution.
 #
 
 """Interfaces to the OSKAR sky model."""
@@ -502,6 +479,27 @@ class Sky(object):
         array[:, 10] *= (180.0 / math.pi) * 3600.0
         array[:, 11] *= (180.0 / math.pi)
         return array
+
+    def to_ds9_regions(self, filename, colour='green', width=1):
+        """Writes a ds9 region file from coordinates in the sky model.
+
+        Args:
+            filename (str): Name of file to write.
+            colour (Optional[str]): Name of region colour. Default 'green'.
+            width (Optional[int]): Line width. Default 1.
+        """
+        self.capsule_ensure()
+        array = _sky_lib.to_array(self._capsule)
+        array[:, 0] *= (180.0 / math.pi)
+        array[:, 1] *= (180.0 / math.pi)
+        with open(filename, "w") as file:
+            file.write('global color=%s dashlist=8 3 width=%d '
+                       'font="helvetica 10 normal roman" select=1 highlite=1 '
+                       'dash=0 fixed=0 edit=1 move=1 delete=1 include=1 '
+                       'source=1\nfk5\n' % (colour, width))
+            for i in range(0, len(array)):
+                file.write('point(%10.6f, %10.6f) # point=x\n' %
+                           (array[i, 0], array[i, 1]))
 
     # Properties
     capsule = property(capsule_get, capsule_set)
