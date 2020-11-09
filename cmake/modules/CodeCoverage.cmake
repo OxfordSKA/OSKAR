37d1eeda2cda488f07da0d1fd7440ffbd008431d
Fred Dulwich <145223+fdulwich@users.noreply.github.com>
2020-11-09 16:31:16 +0000

    Workaround to ignore incorrectly mis-hit function declarations.

--- cmake/modules/CodeCoverage.cmake
+++ cmake/modules/CodeCoverage.cmake
@@ -248,6 +248,9 @@ function(setup_target_for_coverage_lcov)
         # filter collected data to final coverage report
         COMMAND ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --remove ${Coverage_NAME}.total ${LCOV_EXCLUDES} --output-file ${Coverage_NAME}.info
 
+        # Workaround: Ignore incorrectly mis-hit function declarations
+        COMMAND python3 ${PROJECT_SOURCE_DIR}/cmake/modules/fix_coverage.py ${Coverage_NAME}.info ${Coverage_NAME}.info
+
         # Generate HTML output
         COMMAND ${GENHTML_PATH} ${GENHTML_EXTRA_ARGS} ${Coverage_GENHTML_ARGS} -o ${Coverage_NAME} ${Coverage_NAME}.info
 
