517e10237997bd8dc985d5748e9b5df1e68e7d0c
Fred Dulwich <145223+fdulwich@users.noreply.github.com>
2021-10-18 18:23:07 +0100

    Update Docker images to use CUDA 11.4 for Ampere support.

--- docker/oskar-base-dep/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Create a local user on the fly using the LOCAL_USER_ID environment variable.
-USER_ID=${LOCAL_USER_ID:-9001}
-useradd -s /bin/bash -u $USER_ID -o -m oskar
-export HOME=/home/oskar
-exec gosu oskar "$@"
