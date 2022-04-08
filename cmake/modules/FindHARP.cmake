3a0d242399278ce9f8fd2591211bad83de7cffb9
Fred Dulwich <145223+fdulwich@users.noreply.github.com>
2022-04-08 15:12:01 +0100

    HIP-119: Find HARP on Windows.

--- cmake/modules/FindHARP.cmake
+++ cmake/modules/FindHARP.cmake
@@ -2,6 +2,10 @@ include(FindPackageHandleStandardArgs)
 
 find_path(HARP_INCLUDE_DIR harp_beam.h PATH_SUFFIXES include)
 find_library(HARP_LIBRARY harp_beam PATH_SUFFIXES lib)
+if (WIN32)
+    find_file(HARP_DLL harp_beam.dll PATH_SUFFIXES bin)
+    mark_as_advanced(HARP_DLL)
+endif()
 mark_as_advanced(HARP_INCLUDE_DIR HARP_LIBRARY)
 
 find_package_handle_standard_args(HARP
