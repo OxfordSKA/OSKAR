d16f2f0a0faf43c534a460e123be584af22dcde6
Fred Dulwich <145223+fdulwich@users.noreply.github.com>
2022-04-29 12:43:23 +0100

    Fix apps test if not using MS.

--- oskar/apps/test/Test_Interferometer.cpp
+++ oskar/apps/test/Test_Interferometer.cpp
@@ -126,9 +126,11 @@ TEST(apps, test_interferometer_modes)
                 ASSERT_TRUE(sim_settings->set_value(
                         "interferometer/oskar_vis_filename",
                         string(root_name + ".vis").c_str()));
+#ifndef OSKAR_NO_MS
                 ASSERT_TRUE(sim_settings->set_value(
                         "interferometer/ms_filename",
                         string(root_name + ".ms").c_str()));
+#endif
 
                 // Create an interferometer simulator.
                 oskar_Interferometer* sim = oskar_settings_to_interferometer(
