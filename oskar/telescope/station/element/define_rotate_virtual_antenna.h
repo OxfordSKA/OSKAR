c9289eeff225fac6fdba1d2f081492048cae5b51
Fred Dulwich <145223+fdulwich@users.noreply.github.com>
2023-05-22 23:24:00 +0100

    HIP-558: Fix rotation matrix for virtual antenna.

--- oskar/telescope/station/element/define_rotate_virtual_antenna.h
+++ oskar/telescope/station/element/define_rotate_virtual_antenna.h
@@ -10,16 +10,16 @@ KERNEL(NAME) (\
 {\
     KERNEL_LOOP_X(int, i, 0, n)\
     const int j = i + offset;\
-    const FP2 x_theta_ = jones[j].a, x_phi_ = jones[j].b;\
-    const FP2 y_theta_ = jones[j].c, y_phi_ = jones[j].d;\
-    jones[j].a.x = x_theta_.x * cos_angle - x_phi_.x * sin_angle;\
-    jones[j].a.y = x_theta_.y * cos_angle - x_phi_.y * sin_angle;\
-    jones[j].b.x = x_theta_.x * sin_angle + x_phi_.x * cos_angle;\
-    jones[j].b.y = x_theta_.y * sin_angle + x_phi_.y * cos_angle;\
-    jones[j].c.x = y_theta_.x * cos_angle - y_phi_.x * sin_angle;\
-    jones[j].c.y = y_theta_.y * cos_angle - y_phi_.y * sin_angle;\
-    jones[j].d.x = y_theta_.x * sin_angle + y_phi_.x * cos_angle;\
-    jones[j].d.y = y_theta_.y * sin_angle + y_phi_.y * cos_angle;\
+    const FP2 x_theta = jones[j].a, x_phi = jones[j].b;\
+    const FP2 y_theta = jones[j].c, y_phi = jones[j].d;\
+    jones[j].a.x = cos_angle * x_theta.x + sin_angle * y_theta.x; /* X_theta */\
+    jones[j].a.y = cos_angle * x_theta.y + sin_angle * y_theta.y;\
+    jones[j].b.x = cos_angle * x_phi.x + sin_angle * y_phi.x; /* X_phi */\
+    jones[j].b.y = cos_angle * x_phi.y + sin_angle * y_phi.y;\
+    jones[j].c.x = -sin_angle * x_theta.x + cos_angle * y_theta.x; /* Y_theta */\
+    jones[j].c.y = -sin_angle * x_theta.y + cos_angle * y_theta.y;\
+    jones[j].d.x = -sin_angle * x_phi.x + cos_angle * y_phi.x; /* Y_phi */\
+    jones[j].d.y = -sin_angle * x_phi.y + cos_angle * y_phi.y;\
     KERNEL_LOOP_END\
 }\
 OSKAR_REGISTER_KERNEL(NAME)
