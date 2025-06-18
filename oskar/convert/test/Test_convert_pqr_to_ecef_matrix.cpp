17d15ee04df81330bbecd1a998bf037efef52a1f
Fred Dulwich <145223+fdulwich@users.noreply.github.com>
2025-06-18 15:08:39 +0100

    Improve clarity of test.

--- oskar/convert/test/Test_convert_pqr_to_ecef_matrix.cpp
+++ oskar/convert/test/Test_convert_pqr_to_ecef_matrix.cpp
@@ -12,29 +12,32 @@
 #include <cstdlib>
 #include <cstdio>
 
-static void transpose(double m[9])
+static void transpose(const double in[9], double out[9])
 {
-    double t = 0.0;
-    t = m[1]; m[1] = m[3]; m[3] = t;
-    t = m[2]; m[2] = m[6]; m[6] = t;
-    t = m[5]; m[5] = m[7]; m[7] = t;
+    double t = 0.0; // Use temporary, in case in and out are really the same.
+    t = in[1]; out[1] = in[3]; out[3] = t;
+    t = in[2]; out[2] = in[6]; out[6] = t;
+    t = in[5]; out[5] = in[7]; out[7] = t;
+    out[0] = in[0]; // Just copy the diagonals.
+    out[4] = in[4];
+    out[8] = in[8];
 }
 
 TEST(convert_pqr_to_ecef_matrix, station_s8_1)
 {
     // Check conversion matrix for station S8-1.
     // This is the transpose of the answer we need to get.
-    double pqr_to_ecef_for_s8_1[9] = {
+    double ecef_to_pqr_for_s8_1[9] = {
              0.09388869,  0.52638352,  0.84504752,
              0.91113548,  0.29667722, -0.2860328,
             -0.4012693,   0.79680802, -0.45175206
     };
+    double pqr_to_ecef_for_s8_1[9];
     // Transpose because the matrix as written above is actually
     // ecef_to_pqr, and we want it the other way for this test.
     // HOWEVER, the thing that should be written to the PHASED_ARRAY
     // table really is ecef_to_pqr (the values in the order above)!
-    transpose(pqr_to_ecef_for_s8_1);
-    // Now pqr_to_ecef_for_s8_1 is what it says it is.
+    transpose(ecef_to_pqr_for_s8_1, pqr_to_ecef_for_s8_1);
 
     // Input values for S8-1.
     double rotation_angle_rad = 251.3 * M_PI / 180;
