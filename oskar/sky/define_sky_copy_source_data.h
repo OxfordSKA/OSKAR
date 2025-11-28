60c5739fb0d0656a5b4cdd3b85cf5ff0f0fd668a
Fred Dulwich <145223+fdulwich@users.noreply.github.com>
2025-11-28 10:31:06 +0000

    Attempt to fix OpenCL kernel.

--- oskar/sky/define_sky_copy_source_data.h
+++ oskar/sky/define_sky_copy_source_data.h
@@ -2,15 +2,19 @@
 
 #define OSKAR_SKY_COPY_SOURCE_DATA(NAME, FP) KERNEL(NAME) (\
         const int num, GLOBAL_IN(int, mask), GLOBAL_IN(int, indices),\
-        const int num_col, GLOBAL const FP** col_in, GLOBAL FP** col_out\
+        const int num_col, GLOBAL_IN(FP, col_in), GLOBAL_OUT(FP, col_out)\
 )\
 {\
+    /* Casts to try and get OpenCL version working, which does not support */\
+    /* pointers-to-pointers in kernel arguments. */\
+    GLOBAL const FP* const* col_in_ = (const FP* const*) col_in;\
+    GLOBAL FP** col_out_ = (FP**) col_out;\
     KERNEL_LOOP_X(int, i, 0, num)\
     if (mask[i]) {\
         const int i_out = indices[i];\
         _Pragma("unroll")\
         for (int c = 0; c < num_col; ++c) {\
-            (col_out[c])[i_out] = (col_in[c])[i];\
+            (col_out_[c])[i_out] = (col_in_[c])[i];\
         }\
     }\
     KERNEL_LOOP_END\
